c64c3cc33b5a6750b8f8e014938b38d9437b9c06
Antoni <antoni.d996@gmail.com>
2019-12-06 18:46:11 +0700

    controller service

--- src/main/java/com/amigoscode/amigos/Dao/StudentDao.java
+++ src/main/java/com/amigoscode/amigos/Dao/StudentDao.java
@@ -1,10 +1,31 @@
 package com.amigoscode.amigos.Dao;
 
 import com.amigoscode.amigos.Entity.Student;
+import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 
+@Repository
 public class StudentDao {
 
     private static Map<Integer, Student> students;
+
+    static {
+        students = new HashMap<Integer, Student>(){
+            {
+                put(1, new Student(1, "Said", "Computer Science"));
+                put(2, new Student(2, "Alex", "Finance"));
+                put(3, new Student(3, "Anna", "Maths"));
+            }
+        };
+    }
+    public Collection<Student> getAllStudents(){
+        return this.students.values();
+    };
+
+    public Student getStudentById(int id){
+        return this.students.get(id);
+    };
 }
---
+++ src/main/java/com/amigoscode/amigos/Service/StudentService.java
@@ -0,0 +1,23 @@
+package com.amigoscode.amigos.Service;
+
+import com.amigoscode.amigos.Dao.StudentDao;
+import com.amigoscode.amigos.Entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentDao studentDao;
+
+    public Collection<Student> getAllStudents(){
+        return this.studentDao.getAllStudents();
+    }
+
+    public Student getStudentById(int id){
+        return this.studentDao.getStudentById(id);
+    }
+}
